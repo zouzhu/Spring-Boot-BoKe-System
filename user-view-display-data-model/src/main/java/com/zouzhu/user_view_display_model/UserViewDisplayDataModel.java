cd484004189c551d1a63c9ee173aaab8098bb014
zouzhu_pro <zouzhu@net.com.cn>
2019-09-09 15:22:02 +0800

    user-view-display-data-model ---> pom.xml

---
+++ user-view-display-data-model/src/main/java/com/zouzhu/user_view_display_model/UserViewDisplayDataModel.java
@@ -0,0 +1,21 @@
+package com.zouzhu.user_view_display_model;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserViewDisplayDataModel {
+
+    /**
+     *       描述 : 对于用户的请求做出分析, 向其他的服务提供者模块发起请求, 并将得到的数据展示给用户
+     *
+     *       Auther : zouzhu
+     *
+     *       time : 2019/9/9 15:15
+     * **/
+
+    public static void main(String[] args){
+        SpringApplication.run(UserViewDisplayDataModel.class, args);
+    }
+}
